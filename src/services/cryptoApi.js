7589ef882d22260387d0447648a278cce2f23ca6
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-22 20:43:23 +0200

    fetch api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,12 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-/*
-const options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/coins',
-  headers: {
+const cryptoApiHeaders = {
     'X-RapidAPI-Key': '3819bbdfbemsh728d704970277e1p165420jsn747a79b95703',
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-  }
-};
+}
 
-*/
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
