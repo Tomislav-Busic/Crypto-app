938ced3a134ae6c3672650b98fd0f91c1ab72e87
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-22 19:53:56 +0200

    services

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,12 @@
+
+/*
+const options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  headers: {
+    'X-RapidAPI-Key': '3819bbdfbemsh728d704970277e1p165420jsn747a79b95703',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  }
+};
+
+*/
