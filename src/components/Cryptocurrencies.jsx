50dea1a0213593dd0d6c8c98c7830186ac57cfb1
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-23 12:08:02 +0200

    top 10 crypto

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,20 +5,20 @@ import { Card, Col, Row, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const Cryptocurrencies = () => {
-  const {data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const {data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setcryptos] = useState(cryptosList?.data?.coins);
 
   console.log(cryptos);
 
-  if(isFetching){
-    
-  }
+  if(isFetching) return 'Loading...';
+  
 
   return (
     <>
       <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos.map((currency) => (
+        {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
               <Card title={`${currency.rank}. ${currency.name}`}
