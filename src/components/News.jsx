3642afe80f76f448b858902ac53c33f881195efe
Tomislav Busic <tomislav.busic89@gmail.com>
2022-08-23 17:03:29 +0200

    crypto news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,14 +7,42 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImageUrl = 'https://cdn.pixabay.com/photo/2018/01/18/07/31/bitcoin-3089728__480.jpg';
+
 const News = ({simplified}) => {
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 });
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 20 });
 
+  if(!cryptoNews?.value) return 'Loading...';
   console.log(cryptoNews)
-
   
   return (
-    <div>News</div>
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name}</Title>
+                <img style={{maxWidth: '100px', maxHeight: '150px'}} src={news?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news"/>
+              </div>
+              <p>
+                {news.description > 100 
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description
+                }
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImageUrl} alt="news" />
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                </div>
+                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
